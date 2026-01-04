48abcbfa178167c0c9f5693aaae131122b919b4e
Aman Soni <amansoni@Amans-MacBook-Pro.local>
2026-01-04 19:47:29 +0530

    jwt logout done

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -89,5 +89,17 @@ const me = async (req, res) => {
   res.status(200).json(req.user);
 };
 
+const logout = (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    expires: new Date(0), 
+    sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
+  });
 
-module.exports = { signup,login,me };
+  res.status(200).json({ message: "Logged out successfully" });
+};
+
+
+
+module.exports = { signup,login,me,logout };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { signup,login,me } = require("../controllers/controller");
+const { signup,login,me,logout } = require("../controllers/controller");
 const authMiddleware = require("../middlewares/middleware");
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/me", authMiddleware, me);
+router.post("/logout", authMiddleware, logout);
 
 module.exports = router;
-
-
