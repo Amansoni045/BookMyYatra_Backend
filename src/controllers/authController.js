f37c0bbbebde230aa6383003e3cb0be19fcc0320
Aman Soni <amansoni@Amans-MacBook-Pro.local>
2026-01-07 11:23:05 +0530

    bug fixing

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -93,7 +93,7 @@ const logout = (req, res) => {
     res.cookie("token", "", {
         httpOnly: true,
         expires: new Date(0),
-        sameSite: "strict",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         secure: process.env.NODE_ENV === "production",
     });
 
