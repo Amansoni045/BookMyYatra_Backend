6dbbdfc78697e4b8893373f54272deb7d9301542
Aman Soni <amansoni@Amans-MacBook-Pro.local>
2026-01-14 22:59:40 +0530

    fix3

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -11,6 +11,7 @@ const generateToken = (res, userId) => {
     httpOnly: true,
     secure: true,
     sameSite: "none",
+    domain: ".onrender.com",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
