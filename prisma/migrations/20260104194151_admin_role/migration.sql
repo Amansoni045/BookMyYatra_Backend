0eac53496dfbfbcbfe92dab967ae726aa2353ad5
Aman Soni <amansoni@Amans-MacBook-Pro.local>
2026-01-15 10:44:22 +0530

    migrated

--- prisma/migrations/20260104194151_admin_role/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
-
--- AlterTable
-ALTER TABLE "Hotel" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
--- prisma/migrations/20260115050755_migrate/migration.sql
+++ prisma/migrations/20260115050755_migrate/migration.sql
@@ -1,9 +1,13 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
@@ -21,6 +25,7 @@ CREATE TABLE "Hotel" (
     "maxGuests" INTEGER NOT NULL,
     "description" TEXT NOT NULL,
     "services" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Hotel_pkey" PRIMARY KEY ("id")
 );
